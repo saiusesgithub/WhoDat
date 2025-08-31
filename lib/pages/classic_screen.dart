e4af814c51e9159492c1e638b7deb0eca4420818
saiusesgithub <saisrujantesting@gmail.com>
2025-08-31 19:10:15 +0530

    designed the classic screen page

--- lib/pages/classic_screen.dart
+++ lib/pages/classic_screen.dart
@@ -15,9 +15,9 @@ class _ClassicScreenState extends State<ClassicScreen> {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        leading: IconButton.outlined(
+        leading: IconButton(
           onPressed: () {},
-          icon: Icon(Pixel.arrowleft),
+          icon: Icon(Pixel.arrowleft, color: Color.fromRGBO(5, 39, 51, 1.0)),
         ),
         backgroundColor: Color.fromRGBO(39, 246, 163, 1.0),
         title: Text(
@@ -27,6 +27,132 @@ class _ClassicScreenState extends State<ClassicScreen> {
             color: Color.fromRGBO(5, 39, 51, 1.0),
             fontSize: 18,
           ),
+        ),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(25),
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                questionCard(),
+                Column(
+                  spacing: 10,
+                  children: [
+                    Row(
+                      spacing: 10,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [yesButton(), noButton()],
+                    ),
+                    Row(
+                      spacing: 10,
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [maybeButton(), idkButton()],
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget questionCard() {
+    return Card(
+      color: Color.fromRGBO(39, 246, 163, 1.0),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      elevation: 5,
+      margin: EdgeInsets.all(20),
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Text(
+          'Sample Question which is supposed to be really long?',
+          style: TextStyle(color: Color.fromRGBO(5, 39, 51, 1.0), fontSize: 30),
+        ),
+      ),
+    );
+  }
+
+  Widget yesButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(200, 40),
+        backgroundColor: Color.fromRGBO(39, 246, 163, 1.0),
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      child: Text(
+        'Yes',
+        style: TextStyle(
+          fontFamily: 'PressStart2P',
+          color: Color.fromRGBO(5, 39, 51, 1.0),
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+
+  Widget noButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(200, 40),
+        backgroundColor: Color.fromRGBO(39, 246, 163, 1.0),
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      child: Text(
+        'No',
+        style: TextStyle(
+          fontFamily: 'PressStart2P',
+          color: Color.fromRGBO(5, 39, 51, 1.0),
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+
+  Widget maybeButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(200, 40),
+        backgroundColor: Color.fromRGBO(39, 246, 163, 1.0),
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      child: Text(
+        'Maybe',
+        style: TextStyle(
+          fontFamily: 'PressStart2P',
+          color: Color.fromRGBO(5, 39, 51, 1.0),
+          fontSize: 14,
+        ),
+      ),
+    );
+  }
+
+  Widget idkButton() {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        minimumSize: Size(200, 40),
+        backgroundColor: Color.fromRGBO(39, 246, 163, 1.0),
+        padding: EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+      ),
+      child: Text(
+        'i don\'t know',
+        style: TextStyle(
+          fontFamily: 'PressStart2P',
+          color: Color.fromRGBO(5, 39, 51, 1.0),
+          fontSize: 14,
         ),
       ),
     );
